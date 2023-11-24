f1534d25bc59e6e44a6120b41887d997a40ca826
FIVe04 <83167979+FIVe04@users.noreply.github.com>
2023-11-25 02:32:11 +0300

    Update index.js Access-Control-Allow-Origin

--- server/index.js
+++ server/index.js
@@ -22,6 +22,13 @@ mongoose
     .catch((err) => console.log(err));
 
 
+ app.use(function(req,res,next){
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, PATCH");
+    res.header("Access-Control-Allow-Headers", "Accept, Content-Type, Authorization, X-Requested-With");
+    next();
+  });
+
 app.use(
     cors({
         origin: ["https://the-bookshelf-full-stack.vercel.app/"],
